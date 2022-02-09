90fc91613c3e8742bc9c31ae118e0f1696cf5556
Gasyh3 <kevrakt@gmail.com>
2022-02-09 20:12:25 +0100

    Mise en place de la fonction pour récupérer les éléments de l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,10 @@
-//Afficher tout les articles de manière dynamique
-
 //url de l'API
 let url = 'http://localhost:3000/api/products';
 
-//Récupération des éléments de l'API et affichage des items
-fetch(url)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-    let affichage = '<section>';
-    for (let canape of data) {
-        affichage +=
-        `<article>
-        <img src="${canape.imageUrl}" alt="{canape.altTxt}" />
-        <h3 class="productName">${canape.name}</h3> 
-        <p class="productDescription">${canape.description}</p>
-        </article>`
-    }
-    affichage += '</section>';
-    document.querySelector("#items").innerHTML = affichage;
-});
-
+//Récupération des éléments de l'API
+async function getItemsAPI() {
+    let itemsRecuperation = await fetch(url)
+    return await itemsRecuperation.json();
+}
 
 
