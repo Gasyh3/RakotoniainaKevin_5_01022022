5ea803ce238f1a94f1a1d4700b5d49cd038ef236
Gasyh3 <kevrakt@gmail.com>
2022-02-22 14:26:22 +0100

    Séparation des fonctionnalité + version final du code

--- front/js/script.js
+++ front/js/script.js
@@ -1,49 +1,48 @@
-affichageCanapes();
+/**
+ * * ETAPE 3 : Insérer les produits dans la page d'accueil
+ */
 
-//Récupération des éléments de l'API
-async function getItemsAPI() {
-    let itemsRecuperation = await fetch('http://localhost:3000/api/products')
-    return await itemsRecuperation.json();
+getCanapes();
+
+/**
+ * * Récupération des éléments de l'API 
+ */
+function getCanapes () {
+    fetch("http://localhost:3000/api/products/")
+    .then((response) => response.json())
+    .then((dataScript) => displayCanapesOnIndex(dataScript))
+    .catch((error) => console.error(error));
 }
 
-//Affichage des éléments de l'API dans le DOM
-async function affichageCanapes () {
-    let affichage = await getItemsAPI()
-    .then(function (dataAPI) {
-        const canapes = dataAPI;
-        console.table(canapes);
-        for (let canape in canapes) {
+/**
+ * * Manipulation du DOM afin d'afficher les canapés de manière dynamique 
+ * @param {*} dataScript 
+ */
+function displayCanapesOnIndex (dataScript) {
+    const CANAPES = dataScript;
+    for (let canape in CANAPES) {
 
-            //Mise en place de l'élément <a> ETAPE 4
-            let canapeLien = document.createElement("a");
-            document.querySelector(".items").appendChild(canapeLien);
-            canapeLien.href = `product.html?id=${dataAPI[canape]._id}`;
+        //ETAPE 4: Mise en place de l'élément <a> et du href
+        let canapeLien = document.createElement("a");
+        document.querySelector(".items").appendChild(canapeLien);
+        canapeLien.href = `product.html?id=${dataScript[canape]._id}`;
 
-            //Mise en place de l'élément <article>
-            let canapeArticle = document.createElement("article");
-            canapeLien.appendChild(canapeArticle);
+        let canapeArticle = document.createElement("article");
+        canapeLien.appendChild(canapeArticle);
 
-            //Mise en place de l'élément <img>
-            let canapeImg = document.createElement("img");
-            canapeArticle.appendChild(canapeImg);
-            canapeImg.src = dataAPI[canape].imageUrl;
-            canapeImg.alt = dataAPI[canape].altTxt;
+        let canapeImg = document.createElement("img");
+        canapeArticle.appendChild(canapeImg);
+        canapeImg.src = dataScript[canape].imageUrl;
+        canapeImg.alt = dataScript[canape].altTxt;
 
-            //Mise en place de l'élément <h3>
-            let canapeNom = document.createElement("h3");
-            canapeArticle.appendChild(canapeNom);
-            canapeNom.classList.add("productName");
-            canapeNom.innerHTML = dataAPI[canape].name;
+        let canapeNom = document.createElement("h3");
+        canapeArticle.appendChild(canapeNom);
+        canapeNom.classList.add("productName");
+        canapeNom.innerHTML = dataScript[canape].name;
 
-            //Mise en place de l'élément <p>
-            let canapeDescription = document.createElement("p");
-            canapeArticle.appendChild(canapeDescription);
-            canapeDescription.classList.add("productDescription");
-            canapeDescription.innerHTML = dataAPI[canape].description;
-        }
-    })
-    .catch (function(error) {
-        console.log("error");
-        return error;
-    });
+        let canapeDescription = document.createElement("p");
+        canapeArticle.appendChild(canapeDescription);
+        canapeDescription.classList.add("productDescription");
+        canapeDescription.innerHTML = dataScript[canape].description;
+    }
 }
